d8585a1b07ffe7b95ad327242650dbc19e8c2787
shobha9250 <56711324+shobha9250@users.noreply.github.com>
2021-07-29 23:28:46 +0530

    fixed errors

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const express = require('express');
 const path = require('path');
 const hbs= require('hbs');
 const { partials } = require('handlebars');
+const request = require('request');
+const bodyparser= require('body-parser')
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//middleware for body parser
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json())
 
 
 
@@ -26,8 +32,14 @@ app.set('view engine','hbs');
 app.set('views', template_path)
 
 hbs.registerPartials(partials_path);
+hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
 const key= process.env.API_KEY;
+let URL    = 'http://api.openweathermap.org/data/2.5/weather?q='
+let units  = '&units=metric'; 
+
 
 //by default index.html in public folder, if using html
 app.get('', (req,res) => {
@@ -39,13 +51,31 @@ app.get('/about', (req,res) => {
 });
 
 app.get('/weather', (req,res) => {
-    res.render('weather' , {key:key,});
+    res.render('weather' , {obj :false, emptyErr:false, invalidCity:false});
 });
 
 //this is for error page
 app.get('*', (req,res) => {
     res.render('404error' ,{errmsg: 'Oops! Page not found'});
 });
+
+var city;
+
+app.post('/weather', function(req, res, next){
+    if(req.body.readcity === '')
+        return res.render('weather', {obj :false, emptyErr:true, invalidCity:false});
+    city = req.body.readcity;
+    url = URL+city+"&"+units+"&appid="+`${key}`;
+    request(url, function (error, response, obj) {
+        
+        if(error || response.statusCode != 200){
+            return res.render('weather', {obj :false, emptyErr:false, invalidCity:true});
+        }
+        obj = JSON.parse(obj);
+      res.render('weather', {obj : obj,emptyErr:false, invalidCity:false});
+     });
+});
+
 
 app.listen(port , () => {
     console.log(`running at port ${port}`);
--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,8 @@
 
 
 const submitbtn =document.getElementById('submitbtn');
-const outputcityname = document.getElementById('city_name');
-const inputcityname = document.getElementById('cityName');
-const temp_status = document.getElementById('temp_status');
-const temp = document.getElementById('temp');
-
-const inner_layer =document.querySelector('.inner_layer');
-
 const outputday=document.getElementById('day');
 const outputdate=document.getElementById('date');
-
-
-const getInfo = async(event) =>{
-    
-    //prevents refreshing
-    event.preventDefault();
-    let inputcityVal = inputcityname.value;
-    if(inputcityVal === ''){
-        outputcityname.innerText = `that's not a city`;
-        inner_layer.classList.add('data_hide');
-    }
-    else{
-         try{
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${inputcityVal}&units=metric&appid=mykey`;
-            console.log(url);
-            const response = await fetch(url);
-            const data = await response.json();
-            
-            const arrData = [data];     //array of objects
-            
-            outputcityname.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
-            temp.innerText = arrData[0].main.temp;
-            const temp_mood = arrData[0].weather[0].main;
-            
-
-            if(temp_mood === 'Clear'){
-                temp_status.innerHTML = "<i class='fas fa-sun' style='color: #eccc68;'</i>";
-            } else if(temp_mood === 'Clouds'){
-                temp_status.innerHTML = "<i class='fas fa-cloud' style='color: #f1f2f6;'</i>";
-            } else if(temp_mood === 'Rain'){
-                temp_status.innerHTML = "<i class='fas fa-cloud-rain' style='color: #a4b0be;'</i>";
-            } else {
-                temp_status.innerHTML = "<i class='fas fa-sun' style='color: #f1f2f6;'</i>";
-            }
-            inner_layer.classList.remove('data_hide');
-
-         }catch(err) {
-            inner_layer.classList.add('data_hide');
-            outputcityname.innerText = `Please enter a city name`;
-            
-         }
-    }
-}
 
 const getcurrentday = () => {
     let weekday =new Array(7);
@@ -96,4 +46,3 @@ const getcurrentdate = () => {
 
 getcurrentday();
 getcurrentdate();
-submitbtn.addEventListener('click',getInfo);
